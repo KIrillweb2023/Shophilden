b0f0430c4702bc4f6e68e22e4e0c6b6e8a1ccda7
unknown <webrazrabotka72@gmail.com>
2024-03-13 15:20:53 +0300

    commit

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('hello');
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
